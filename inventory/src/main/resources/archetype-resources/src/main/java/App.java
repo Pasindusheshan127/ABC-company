c694a52947437d3f73d40bae984a430d541a6878
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2025-06-04 14:36:54 +0530

    create abc company api

---
+++ inventory/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.abcCompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
